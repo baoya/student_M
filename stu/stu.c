32d3a722412af562f9ee308fda074dc7b8adb18e
baoya <957653748@qq.com>
2016-10-01 22:23:46 +0800

    第二次提交

--- stu/stu.c
+++ stu/stu.c
@@ -1,13 +1,16 @@
 #include"stdio.h"
 #include"stdlib.h"
 #include"malloc.h"
+//#include"string.h"
 
-#define LEN sizeof(struct student)			//实现代码的简写
+
+#define LEN sizeof(struct student)			//宏定义 LEN = sizeof(struct student)
 
 struct student *creat();					//声明创建链表的函数
 void print(struct student *head);			//声明输出链表的函数
-
-int menu();									//声明界面函数
+struct student *loadinfo();					//加载文件内容到链表
+void savequit(struct student *head);		//将链表内容写入文件
+void menu();								//声明界面函数
 int n=1;									//从第一个学生开始,学生人数统计,控制循环输出
 
 struct student								//定义学生信息这一结构体
@@ -16,58 +19,83 @@ struct student								//定义学生信息这一结构体
 	char name[20];							//学生姓名
 	char sex[10];							//学生性别
 	int age;								//学生年龄
-	struct student *next;					//指向下一个学生信息
+	struct student *next;					//指向下一个学生信息地址
 	
 };
 
 void main()
 {
 	int number;				//定义对应的功能选择项
-	
-	struct student *stu;	//定义*stu,用来指向创建的链表
-	
+	FILE *fp;
+	struct student *stu,*head;	//定义*stu,用来指向创建的链表
+	stu = NULL;
+	head =NULL;
 	while(1)
 	{
-		
-		menu();
-		scanf("%d",&number);
-		switch(number)
+		if((fp = fopen("data.txt","r"))==NULL)
 		{
-		case 1:				//录入学生信息
-			stu =creat();
-			
-			break;
-		case 2:				//删除学生信息
-			
-			break;
-		case 3:
-			
-			break;
-		case 4:				//查询学生信息
-			print(stu);
-			break;
-		case 5:				//退出
-			exit(0);
-			break;
-		default:
-			printf("正在开发!\n");
-			break;
-			
+			//	if(stu == NULL)//判断数据库是否存有数据
+			//	{
+			char cls = 'y';
+			printf("┏┳┳┳┳┳┳┳┳┳┳┳┳┳┳┳┳┳┳┳┓\n");
+			printf("┣ 数据库没有任何学生信息,请初始化数据库┫\n");
+			printf("┗┻┻┻┻┻┻┻┻┻┻┻┻┻┻┻┻┻┻┻┛\n");
+			stu = creat();
+			savequit(stu);
+			free(stu);
+			printf("录入完毕,按y清屏,输入任意键进入主界面: ");
+			scanf("%s",&cls);
+			if(cls=='y' || cls=='Y')
+				system("cls");//清屏
+		}
+		else
+		{
+			head=loadinfo();
+			menu();
+			scanf("%d",&number);
+			switch(number)
+			{
+			case 1:				//增加学生信息
+				printf("正在开发!\n");
+				break;
+			case 2:				//删除学生信息
+				printf("正在开发!\n");
+				break;
+			case 3:
+				printf("正在开发!\n");
+				break;
+			case 4:				//输出所有学生信息
+				print(head);
+				break;
+			case 5:				//保存
+				savequit(head);
+				free(head);
+				break;
+			case 6:				//退出
+				exit(0);
+				break;
+			default:
+				printf("正在开发!\n");
+				break;
+				
+				
+			}
 		}
 	}	
 }
 
-int menu()
+void menu()
 {
-	printf("■□■□■□---学生管理系统---■□■□■□\n\n");
+	printf("\n\n■□■□■□--学生管理系统--■□■□■□■\n\n");
 	printf("  ▁▂▃▄▅ 1.增加学生信息 ▅▄▃▂▁\n");
 	printf("  ▁▂▃▄▅ 2.删除学生信息 ▅▄▃▂▁\n");
 	printf("  ▁▂▃▄▅ 3.修改学生信息 ▅▄▃▂▁\n");
-	printf("  ▁▂▃▄▅ 4.查询学生信息 ▅▄▃▂▁\n");
-	printf("  ▁▂▃▄▅ 5.退出         ▅▄▃▂▁\n\n");
+	printf("  ▁▂▃▄▅ 4.输出学生信息 ▅▄▃▂▁\n");
+	printf("  ▁▂▃▄▅ 5.保存学生信息 ▅▄▃▂▁\n");
+	printf("  ▁▂▃▄▅ 6.退出         ▅▄▃▂▁\n\n");
+	printf("■□■□■□■□■□■□■□■□■□■□■\n\n");
 	
-	printf("请输入1-5进行你的操作: ");
-	return 0;
+	printf("请输入1-6进行你的操作: ");
 }
 
 struct student *creat()
@@ -82,10 +110,10 @@ struct student *creat()
 	
 	printf("正在录入第%d个学生:\n",n);
 	
-											/*
-											这里不用循环,直接录入第一个学生信息,
-											后面要把第一个学生信息给head.
-											*/
+	/*
+	这里不用循环,直接录入第一个学生信息,
+	后面要把第一个学生信息给head.
+	*/
 	
 	printf("输入学号: ");
 	scanf("%s",&p1->id);
@@ -111,10 +139,10 @@ struct student *creat()
 		p1 = (struct student *)malloc(LEN); //p1开辟新节点
 		if(n<=len)
 		{
-			/*
-				我把n递增放在了上面,第一次录入的信息地址需要放入头节点,所以后面会有多
-				执行一次的情况出现,因为程序还是要走完while才退出,但多的这次无法再通过
-				循环写入链表,所以我利用if捕获n>len这次,捕获到就退出.
+		/*
+		我把n递增放在了上面,第一次录入的信息地址需要放入头节点,所以后面会有多
+		执行一次的情况出现,因为程序还是要走完while才退出,但多的这次无法再通过
+		循环写入链表,所以我利用if捕获n>len这次,捕获到就退出.
 			*/
 			printf("正在录入第%d个学生:\n",n);
 			
@@ -134,26 +162,83 @@ struct student *creat()
 		}
 		
 	}
-	p2->next = NULL;
-	//	free(p1);
-	//	p1 = NULL;
+	p2->next = NULL;			//单向链表的最后一个节点要指向NULL
 	return head;
 }
-void print(struct student *head)
+
+void print(struct student *head)//链表输出函数
 {	
+	char cls='y';
 	struct student *p;
-	printf("\n目前数据库里共有%d名学生的信息:\n",n-1);
+	printf("\n◆◆◆目前数据库里共有%d名学生的信息◆◆◆\n",n-1);
 	p=head;
-	if(head!=NULL)
+	if(head!=NULL)				//只要不是空链表就输出链表中的值
 	{
 		
-		printf("\t学号\t姓名\t性别\t年龄\n");
+		printf("学号\t姓名\t性别\t年龄\n");
 		while(p!=NULL)
 		{
 			
-			printf("\t%s\t%s\t%s\t%d\n",p->id,p->name,p->sex,p->age);
-			p=p->next;
+			printf("%s\t%s\t%s\t%d\n",p->id,p->name,p->sex,p->age);
+			p=p->next;			//转移到下一个节点继续输出,直到p=NULL为止
 		}
 	}
 	printf("\n\n");
+	if(cls=='y' || cls=='Y')
+	{
+		printf("输出完毕,按y清屏,输入任意键返回主界面: ");
+		scanf("%s",&cls);
+		if(cls=='y' || cls=='Y')
+			system("cls");
+	}
+	printf("\n");
+}
+
+void savequit(struct student *head)//链表数据保存到文本文件
+{
+	struct student *p1;
+	FILE *fp;
+	char filename[] = "data.txt";
+	p1 = head;
+	
+	if(NULL == (fp = fopen(filename,"w")))//判断文件是否存在且可读
+	{
+		printf("发生错误!");
+		exit(0);
+	}
+	
+	while(p1 != NULL)
+	{
+		
+		fprintf(fp,"%s\t%s\t%s\t%d\r",p1->id,p1->name,p1->sex,p1->age);
+		p1 = p1->next;
+	}
+	fclose(fp);
+}
+
+struct student *loadinfo()//文本文件输出到链表中
+{	
+	char filename[] = "data.txt";
+	FILE *fp;
+	struct student *head,*p1;
+	
+	head = (struct student *)malloc(LEN);
+	p1 = head;
+	p1 -> next = NULL;
+	
+	if(NULL == (fp = fopen(filename,"r")))
+	{
+		printf("发生错误!");
+		exit(0);
+	}
+	while(!feof(fp))
+	{
+		fscanf(fp,"%s%s%s%d",&p1->id,&p1->name,&p1->sex,&p1->age);
+		
+		p1->next = (struct student *)malloc(LEN);
+		p1 = p1->next;
+		p1->next = NULL;
+	}
+	fclose(fp);
+	return head;
 }
